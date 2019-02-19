e201f1f3e27b3586d6adcb958bc561db19f6f5a6
godanyakosdaniel <47603956+godanyakosdaniel@users.noreply.github.com>
2019-02-19 20:23:25 +0100

    Add files via upload

---
+++ MentalArithmetic/MentalArithmetic/Structs/Number.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MentalArithmetic
+{
+    struct Number
+    {
+        float generatedNumber;
+        Random r;
+
+        public float GeneratedNumber { get => generatedNumber; private set => generatedNumber = value; }
+
+        public Number(int min, int max) : this()
+        {
+            r = new Random();
+            GeneratedNumber = r.Next(min,max);
+        }
+
+        public override string ToString()
+        {
+            return GeneratedNumber.ToString() ;
+        }
+    }
+}
